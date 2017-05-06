0a29a1047f7cf862ceaafbd11beb97d0918a7c9c
LaitNoisette <meyerfranckpro@gmail.com>
2017-05-06 22:59:52 +0200

    Test Connexion BDD

--- index.php
+++ index.php
@@ -4,23 +4,36 @@
 // http://jc.etiemble.free.fr/abc/index.php/realisations/trucs-astuces/postgresql-wamp
 
 
-
+echo phpinfo();
 
 //Connexion à la base de donnée pgsql appelé postgres, avec l'utuilisateur julien et mot de passe Morasin
-  $c=pg_connect ("dbname=postgres user=julien password=Morasin");
+ /* $c=pg_connect ("host=localhost dbname=meyer user=meyer");
 
   if($c==true)
   {
     echo "julien ok";
-  }
+  }*/
 
+$dsn = "pgsql:host=localhost;port=5432;dbname=meyer;user=meyer";
+try{
+	// create a PostgreSQL database connection
+	$conn = new PDO($dsn);
+
+	// display a message if connected to the PostgreSQL successfully
+	if($conn){
+		echo "Connected to the <strong>$dsn</strong> database successfully!";
+	}
+}catch (PDOException $e){
+	// report error message
+	echo $e->getMessage();
+}
 
 //Test d'une première requete qui récupère les info de la table membres ou le nom = julien
-
-   $r=pg_exec ($c , "SELECT * from membres where nom='julien' ");
+/*
+   $r=pg_exec ($c , "SELECT * from membres where nom='Hug' ");
    for ($i=0; $i<pg_numrows($r); $i++) {
      $l=pg_fetch_array($r,$i);
      echo $l["utilisateur"]." <B>".$l["mail"]."</B>.\n";
    }
-
+*/
  ?>
